e02331bc1da8c26fbb8fb3de7794a9074c31e1b3
ced <ced>
2017-03-21 17:59:27 +0100

    early prototype development

--- public/packages/resiexchange/apps/src/resiexchange.module.js
+++ public/packages/resiexchange/apps/src/resiexchange.module.js
@@ -421,26 +421,4 @@ var resiway = angular.module('resiexchange', [
         
         
     }
-])
-
-.controller('homeController', ['$http', '$rootScope', function($http, $rootScope) {
-    var ctrl = this;
-
-    console.log('home controller');  
-    
-    $http.get('index.php?get=resiexchange_stats')
-    .then(
-    function successCallback(response) {
-        var data = response.data;
-        if(typeof response.data.result == 'object') {
-            ctrl.count_questions = data.result['resiexchange.count_questions'];
-            ctrl.count_answers = data.result['resiexchange.count_answers'];
-            ctrl.count_comments = data.result['resiexchange.count_comments'];
-            ctrl.count_users = data.result['resiway.count_users'];            
-        }
-    },
-    function errorCallback() {
-        // something went wrong server-side
-    }); 
-    
-}]);
+]);
