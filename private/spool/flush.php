1a28f268bf88ed6cab9550ef0095d7fbe7a65947
cedricfrancoys <cedricfrancoys@gmail.com>
2017-04-08 16:17:24 +0200

    beta development

--- private/spool/flush.php
+++ private/spool/flush.php
@@ -15,6 +15,7 @@ set_time_limit(0);
 // this utility script uses qinoa library
 // and requires file config/config.inc.php
 require_once('../qn.lib.php');
+require_once('../resi.api.php');
 
 $messages_folder = '../spool';
 
@@ -25,6 +26,9 @@ set_silent(false);
 try {
     $files = scandir($messages_folder);
 
+    // first pass: group messages by user_id (to send all notifications since last flush at once)
+    $spool = [];
+    
     foreach($files as $file) {
         if(in_array($file, ['.', '..'])) continue;
         
@@ -37,40 +41,69 @@ try {
         // retrieve file full path
         $filename = $messages_folder.'/'.$file;
         
-        $om = &ObjectManager::getInstance();        
-        // get login from user_id 
-        $res = $om->read('resiway\User', $user_id, ['login'] );
+        if(!isset($spool[$user_id])) $spool[$user_id] = [];
+        $spool[$user_id][] = $filename;
+    }
 
-        if($res < 0 || !isset($res[$user_id])) throw new Exception(sprintf("user_unidentified (%d, %s)", $user_id, $file), QN_ERROR_NOT_ALLOWED);   
-        $user_data = $res[$user_id];
 
-        // read file content
-        if( !($json = @file_get_contents($filename, FILE_TEXT)) ) continue;
-        $params = json_decode($json, true);
+    foreach($spool as $user_id => $filenames) { 
 
-        if(!isset($params['subject']) || !isset($params['body'])) {
-            unlink($filename); 
-            continue;
-        }
 
+        // retrieve user data
+        $user_data = ResiAPI::loadUserPrivate($user_id);
+        if($user_data < 0) throw new Exception(sprintf("user_unidentified (%d, %s)", $user_id, $file), QN_ERROR_NOT_ALLOWED);   
+        
+        // send daily report after 7 PM
+        
+        // send weekly report friday (after 7 PM)
+
+        
+        $subject = '';
+        $content = '';
+        $count_notifications = count($filenames);
+        if($count_notifications > 1) {
+            $subject = 'ResiWay - activité sur ton compte';
+        }        
+        // build content
+        foreach($filenames as $filename) {
+            // read file content
+            if( !($json = @file_get_contents($filename, FILE_TEXT)) ) continue;
+            $params = json_decode($json, true);
+
+            if(!isset($params['subject']) || !isset($params['body'])) {
+                unlink($filename); 
+                continue;
+            }
+            
+            if($count_notifications > 1) {
+                $content = '<b>'.$params['subject']."</b><br />\n";
+                $content .= $params['body']."<br />\n";
+            }
+            else {
+                $subject = $params['subject'];
+                $content = $params['body'];
+            }
+        }       
+        
         $transport = Swift_SmtpTransport::newInstance(EMAIL_SMTP_HOST, EMAIL_SMTP_PORT, "ssl")
                     ->setUsername(EMAIL_SMTP_ACCOUNT_USERNAME)
                     ->setPassword(EMAIL_SMTP_ACCOUNT_PASSWORD);
 
-        $message = Swift_Message::newInstance($params['subject'])
+        $message = Swift_Message::newInstance($subject)
                     ->setFrom(array(EMAIL_SMTP_ACCOUNT_USERNAME => 'ResiWay'))
                     ->setTo(array($user_data['login']))
                     // some webmail require a text/plain part as default content
-                    ->setBody($params['body'])
+                    ->setBody($content)
                     // in most cases, if a text/html part is found it will be displayed by default
-                    ->addPart($params['body'], 'text/html');
+                    ->addPart($content, 'text/html');
                     
         $mailer = Swift_Mailer::newInstance($transport);
 
         $mailer->send($message);
-        
-        // remove file once processed                
-        unlink($filename);    
+        foreach($filenames as $filename) {
+            // remove files once processed                
+            unlink($filename);    
+        }
     }
 }
 catch(Exception $e) {
