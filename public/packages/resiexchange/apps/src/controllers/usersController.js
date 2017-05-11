c73c95bfa31ba9ab103fe739e1ea18860195401a
cedricfrancoys <cedricfrancoys@gmail.com>
2017-05-11 12:13:43 +0200

    added users list

---
+++ public/packages/resiexchange/apps/src/controllers/usersController.js
@@ -0,0 +1,54 @@
+angular.module('resiexchange')
+
+.controller('usersController', [
+    'users', 
+    '$scope',
+    '$rootScope',    
+    '$http',
+    function(users, $scope, $rootScope, $http) {
+        console.log('users controller');
+
+        var ctrl = this;
+
+        // @data model
+        ctrl.config = {
+            items: users,
+            total: -1,
+            currentPage: 1,
+            previousPage: -1,
+            limit: 30,
+            domain: [],
+            loading: false
+        };
+        
+        ctrl.load = function(config) {
+            if(config.currentPage != config.previousPage) {
+                config.previousPage = config.currentPage;
+                // trigger loader display
+                if(config.total > 0) {
+                    config.loading = true;
+                }
+                $http.post('index.php?get=resiway_user_list&order=reputation', {
+                    domain: config.domain,
+                    start: (config.currentPage-1)*config.limit,
+                    limit: config.limit,
+                    total: config.total
+                }).then(
+                function successCallback(response) {
+                    var data = response.data;
+                    config.items = data.result;
+                    config.total = data.total;
+                    config.loading = false;
+                },
+                function errorCallback() {
+                    // something went wrong server-side
+                });
+            }
+        };
+        
+        
+        // @init
+        ctrl.load(ctrl.config);
+        
+    }
+]);
