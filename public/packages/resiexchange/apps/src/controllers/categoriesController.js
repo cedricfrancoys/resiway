39f780c9203b79ac764a6a744f9e1526170f32b0
cedricfrancoys <cedricfrancoys@gmail.com>
2017-07-02 18:08:37 +0200

    limit categories listing to non-empty categories

--- public/packages/resiexchange/apps/src/controllers/categoriesController.js
+++ public/packages/resiexchange/apps/src/controllers/categoriesController.js
@@ -8,8 +8,6 @@ angular.module('resiexchange')
         console.log('categories controller');
 
         var ctrl = this;
-
-        config.loading = true;
         
         // @data model
         ctrl.config = {
@@ -19,7 +17,7 @@ angular.module('resiexchange')
             previousPage: -1,
             limit: 30,
             domain: [],
-            loading: false
+            loading: true
         };
 
         switch($rootScope.config.application) {
