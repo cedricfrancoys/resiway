e3c83e3609c46a57ce152d826f5b4ca3cecefb62
cedricfrancoys <cedricfrancoys@gmail.com>
2017-11-24 14:53:39 +0100

    init social login integration

---
+++ public/packages/resiway/apps/oauth.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr" ng-app="resiexchange" id="top" ng-controller="rootController as rootCtrl">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+        <meta name="title" content="ResiWay - La plateforme pour la résilience">        
+        <meta name="description" content="L'association ResiWay supporte des outils collaboratifs pour le partage et la diffusion de savoirs libres vers l'autonomie et la résilience">
+
+
+        <!-- scripts -->
+        <?php echo file_get_contents("packages/resiexchange/apps/views/parts/scripts.html"); ?>
+        <!-- styles -->
+
+    </head>
+
+
+    <body class="ng-cloak"></body>
+    
+</html>
