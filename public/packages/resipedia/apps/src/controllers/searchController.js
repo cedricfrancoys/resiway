d0802b81a9c2a2d69c7a49caff486b9cd75e0e34
cedricfrancoys <cedricfrancoys@gmail.com>
2017-12-18 13:13:44 +0100

    added search view

---
+++ public/packages/resipedia/apps/src/controllers/searchController.js
@@ -0,0 +1,53 @@
+angular.module('resipedia')
+
+.controller('searchController', [
+    'search', 
+    '$scope',
+    '$rootScope',
+    '$route',
+    '$http',
+    '$httpParamSerializerJQLike',
+    '$window',
+    function(search, $scope, $rootScope, $route, $http, $httpParamSerializerJQLike, $window) {
+        console.log('search controller');
+
+        var ctrl = this;
+
+        // @data model
+        angular.merge(ctrl, {
+            search: {
+                items: search,
+                total: $rootScope.search.total,
+                currentPage: 1,
+                previousPage: -1,                
+                limit: $rootScope.search.criteria.limit
+            }
+        });
+
+        ctrl.load = function() {
+            if(ctrl.articles.currentPage != ctrl.articles.previousPage) {
+                ctrl.articles.previousPage = ctrl.articles.currentPage;
+                // reset objects list (triggers loader display)
+                ctrl.articles.items = -1;
+                $rootScope.search.criteria.start = (ctrl.articles.currentPage-1)*ctrl.articles.limit;
+                
+                $http.get('index.php?get=resiway_search&'+$httpParamSerializerJQLike($rootScope.search.criteria))
+                .then(
+                    function successCallback(response) {
+                        var data = response.data;
+                        if(typeof data.result != 'object') {
+                            ctrl.search.items = [];
+                        }
+                        ctrl.search.items = data.result;
+                        $window.scrollTo(0, 0);
+                    },
+                    function errorCallback(response) {
+                        // something went wrong server-side
+                        return [];
+                    }
+                );
+            }
+        };
+        
+    }
+]);
